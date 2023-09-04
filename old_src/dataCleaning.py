57201d258bd139490ec34e19cd736fbb512ff33b
Koh Jun Dong <KohComputer@gmail.com>
2023-09-05 02:01:12 +0800

    More cleanup

--- old_src/dataCleaning.py
+++ old_src/dataCleaning.py
@@ -1,15 +1,12 @@
-import numpy as np
-import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style="darkgrid")
-
-
 import string
 import warnings
-warnings.filterwarnings('ignore')
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="darkgrid")
 
 # set display options to show all columns
 pd.set_option('display.max_columns', None)
@@ -38,46 +35,43 @@ print('\n')
 for col in dataset_columns:
     print('{} column missing values: {}'.format(col, dataset[col].isnull().sum()))
     
-#find duplicated row
+# find duplicated row
 
-#get all possible genre value
+# get all possible genre value
 print('number of genre : {}'.format(dataset.track_genre.nunique()))
 print(dataset.track_genre.unique())
 
-#TODO
-#drop row where genre=sleep
-#drop loudness column
-#normilize popularity using min-max normalixzatioin
-#binarize explicite
+# TODO
+# drop row where genre=sleep
+# drop loudness column
+# normilize popularity using min-max normalixzatioin
+# binarize explicite
 
-#normalize data betweeb [0-1]
+# normalize data between [0-1]
 def minMax_normalization(column):
     clean_dataset[column] = (clean_dataset[column] - clean_dataset[column].min()) / (clean_dataset[column].max() - clean_dataset[column].min())
     return clean_dataset[column]       
 
-#backup data 
+# backup data 
 clean_dataset=dataset.copy()
 
-#drop dup data
+# drop dup data
 clean_dataset.drop_duplicates(subset=['track_id'],inplace=True)
 
-#drop loudness column
+# drop loudness column
 clean_dataset.drop('loudness',axis=1,inplace=True)
 
-#drop the song where genre is sleep since there is mostly asmr like song
+# drop the song where genre is sleep since there is mostly asmr like song
 clean_dataset.drop(clean_dataset.index[clean_dataset['track_genre'] == 'sleep'], inplace=True)
 
-#remove row where tempo is 0
+# remove row where tempo is 0
 clean_dataset.drop(clean_dataset.index[clean_dataset['tempo'] == 0], inplace=True)
 
-#normalize popularity
+# normalize popularity
 minMax_normalization('popularity')
 
-#binarize explicit True False to 1 and 0
+# binarize explicit True False to 1 and 0
 clean_dataset["explicit"] = clean_dataset["explicit"].astype(int)
 
-#export
+# export
 clean_dataset.to_csv('clean_data.csv',index=False)
-
-
-
